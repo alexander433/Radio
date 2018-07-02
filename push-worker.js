28caf44629c6889b137f93d8a698dc51cf8df3e7
alexander433 <39641331+alexander433@users.noreply.github.com>
2018-07-02 12:04:33 -0600

    Add files via upload

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1530554415.33
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://i1.sndcdn.com/avatars\u002D000435760185\u002Dndhnb0\u002Dt500x500.jpg',
+  defaultTitle: 'Radio planeta tierra',
+  defaultActionURL: 'https://alexander433.github.io/Radio/',
+  appKey: '3eAqs2hLQqi1qLda5Vzkuw',
+  token: 'MTozZUFxczJoTFFxaTFxTGRhNVZ6a3V3OjY2Rk9Zck1nMU14ZWZkb2RaQk9mY0NBenBVRmNjeHdwb3IzOHdWZDYtNG8',
+  vapidPublicKey: 'BHiAT75YWveWHttOnuF36s1iI7EUUY17JmZbfySrHntGjL75XQzPZcF_aBY9NxGNt23OG-hcPVkGdfGbnVyCPmk='
+})
